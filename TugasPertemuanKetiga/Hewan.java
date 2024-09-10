170401f20f640a933375fbab9480a5923ef6e21c
hasnaanabiyyu <hasnaanabiyyu@gmail.com>
2024-09-10 13:23:26 +0700

    Add files via upload

---
+++ TugasPertemuanKetiga/Hewan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package TugasPertemuanKetiga;
+
+/**
+ *
+ * @author M. HASNAN AL ABIYYU
+ */
+public abstract class Hewan {
+
+    abstract public void BerkembangBiak();
+
+    public void Bernafas() {
+        System.out.println("Saya Bernafas");
+    }
+
+    
+}
+
