6fc4f80245a74998f0c42dd02c31df7d9dec45b4
hasnaanabiyyu <hasnaanabiyyu@gmail.com>
2024-10-02 05:33:50 +0700

    Add files via upload

---
+++ SimulasiUts/ConnectDb.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo.simulasi.uts;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author M. HASNAN AL ABIYYU
+ */
+
+public class ConnectDb {
+
+   private Connection connection;
+    private static final ConnectDb connectionHelper = new ConnectDb();
+
+    public static ConnectDb getConnectDb() {
+        return connectionHelper;
+    }
+
+    public Connection getConnection () {
+        if (connection ==  null) {
+            try {
+                final String URL = "jdbc:postgresql://localhost:5432/Pbo";
+                final String USER = "postgres";
+                final String PASSWORD = "Hasnan270206*";
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connection established");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return connection;
+    }    
+}
+
